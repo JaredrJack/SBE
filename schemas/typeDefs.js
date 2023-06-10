9a7dd884e8c401131e75ff1d1e24e5967109e276
jared <jaredrjack93@gmail.com>
2023-06-10 15:50:45 -0400

    finished schemas folder

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,5 +1,77 @@
-const typeDefs = require('./typeDefs');
+const { gql } = require("apollo-server-express");
 
-const resolvers = require('./resolvers');
+const typeDefs = gql`
 
-module.exports = {typeDefs, resolvers};
+type Query {
+
+    me: User
+
+}
+
+type Mutation {
+
+    login(email: String!, password: String!): Auth 
+
+    addUser(username: String!, email: String!, password: String!): Auth
+
+    saveBook(input: BookInput): User
+
+    removeBook(bookId: String!): User
+
+}
+
+type User {
+
+    _id: ID
+
+    username: String
+
+    email: String
+
+    bookCount: Int 
+
+    saveBook: [Book]
+
+}
+
+type Book {
+    bookId: String!
+
+    authors: [String]
+
+    description: String
+
+    title: String!
+
+    image: String
+
+    link: String
+}
+
+type Auth {
+
+    token: ID!
+
+    users: User
+
+}
+
+input BookInput {
+
+    bookId: String
+
+    authors: [String]
+
+    description: String
+
+    title: String
+
+    image: String
+
+    link: String
+
+}
+
+`;
+
+module.exports = typeDefs;
