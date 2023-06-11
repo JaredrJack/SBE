fe474ccc3c4e46e9ca0d9c09cb1669b8d061f096
jared <jaredrjack93@gmail.com>
2023-06-10 21:24:06 -0400

    mutation and query.js

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,71 @@
+import { gql } from "@apollo/client";
+
+export const LOG_IN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        savedBooks {
+          bookId
+          authors
+          image
+          link
+          description
+          title
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookdata: inputBook!) {
+    saveBook ( bookdata: $bookdata)
+        {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
+
+export const DEL_BOOK = gql`
+mutation delBook($bookId: String!){
+  delBook (bookId: $bookId) {
+    _id
+    username
+    email
+    savedBooks {
+      bookId
+      authors
+      image
+      link
+      description
+      title
+    }
+  }
+}`
